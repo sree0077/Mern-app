0aa9775d3e004b97bcdaf173a797cdf11bf742b8
sree0077 <sreerajkasaragod007@gmail.com>
2025-09-09 00:55:11 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import { connectDB } from './config/db.js';
+
+dotenv.config()
+
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("server is running")
+})
+
+app.listen(5000,()=>{
+    connectDB();
+    console.log("server started  at http://localhost:5000");
+})
