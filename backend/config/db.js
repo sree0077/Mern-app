5ddebc8782da851f2f73eb7c1cbe3d206c69682d
sree0077 <sreerajkasaragod007@gmail.com>
2025-09-09 14:21:01 +0530

    build routes for backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,6 +5,7 @@ export const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`MongoDB connected: ${conn.connection.host}`)
     }catch (error) {
+        
         console.log(error);
         process.exit(1);
     }
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db.js';
+import todoRoutes from './routes/todo.route.js'
 
 dotenv.config()
 
@@ -9,6 +10,8 @@ const app = express();
 app.get("/",(req,res)=>{
     res.send("server is running")
 })
+
+app.use("/api/todos", todoRoutes)
 
 app.listen(5000,()=>{
     connectDB();
